975f66ba64a2bea94889a167c6926d08ad1e8536
Guohui Xiao <gh.xiao@gmail.com>
2013-02-03 00:10:17 +0100

    update skim command line

--- init.el
+++ init.el
@@ -38,7 +38,8 @@
 (setq-default tab-width 4)
 (setq ring-bell-function 'ignore)
 (if (string= system-type "darwin")
-    (set-default-font "Monoca 12")
+;    (set-default-font "Monoca 12")
+    (set-default-font "Menlo 12")
 )
 (menu-bar-mode 1) 
 
@@ -132,17 +133,18 @@
 ;; option -b highlights the current line; option -g opens Skim in the
 ;; background 
   (setq TeX-view-program-list '(("PDF Viewer" 
-                               "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b"))) 
+                               "/Applications/Skim.app/Contents/SharedSupport/displayline -b %n %o %b"))) 
 )
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(TeX-command-list (quote (("LaTeX" "%`%l%(mode)%' %t" TeX-run-TeX nil (latex-mode doctex-mode) :help "Run LaTeX") ("XeLaTeX" "xelatex %s" TeX-run-TeX nil t) ("TeX" "%(PDF)%(tex) %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil (plain-tex-mode texinfo-mode ams-tex-mode) :help "Run plain TeX") ("Makeinfo" "makeinfo %t" TeX-run-compile nil (texinfo-mode) :help "Run Makeinfo with Info output") ("Makeinfo HTML" "makeinfo --html %t" TeX-run-compile nil (texinfo-mode) :help "Run Makeinfo with HTML output") ("AmSTeX" "%(PDF)amstex %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil (ams-tex-mode) :help "Run AMSTeX") ("ConTeXt" "texexec --once --texutil %(execopts)%t" TeX-run-TeX nil (context-mode) :help "Run ConTeXt once") ("ConTeXt Full" "texexec %(execopts)%t" TeX-run-TeX nil (context-mode) :help "Run ConTeXt until completion") ("BibTeX" "bibtex %s" TeX-run-BibTeX nil t :help "Run BibTeX") ("View" "%V" TeX-run-discard-or-function t t :help "Run Viewer") ("Print" "%p" TeX-run-command t t :help "Print the file") ("Queue" "%q" TeX-run-background nil t :help "View the printer queue" :visible TeX-queue-command) ("File" "%(o?)dvips %d -o %f " TeX-run-command t t :help "Generate PostScript file") ("Index" "makeindex %s" TeX-run-command nil t :help "Create index file") ("Check" "lacheck %s" TeX-run-compile nil (latex-mode) :help "Check LaTeX file for correctness") ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help "Spell-check the document") ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files") ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files") ("Other" "" TeX-run-command t t :help "Run an arbitrary command"))))
- '(ecb-options-version "2.40")
- '(safe-local-variable-values (quote ((TeX-master . \.\./main) (reftex-plug-into-AUCTeX . t) (TeX-auto-save . t) (TeX-parse-self . t) (TeX-debug-bad-boxes . t) (whitespace-line-column . 80) (lexical-binding . t)))))
+ '(safe-local-variable-values 
+   (quote ((TeX-master . \.\./main) 
+           (reftex-plug-into-AUCTeX . t) 
+           (TeX-auto-save . t) 
+           (TeX-parse-self . t) 
+           (TeX-debug-bad-boxes . t) 
+           (whitespace-line-column . 80) 
+           (lexical-binding . t)))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
