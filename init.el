c397f0073071a90e847159eae6ab6ee620e6070a
Guohui Xiao <gh.xiao@gmail.com>
2013-04-15 23:56:14 +0200

    add n3-mode

--- init.el
+++ init.el
@@ -329,6 +329,26 @@
                 (font-lock-mode 1))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; N3 Mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; https://github.com/kurtjx/n3-mode-for-emacs
+
+(add-to-list 'load-path "~/.emacs.d/ventor/n3-mode.el")
+(autoload 'n3-mode "n3-mode" "Major mode for OWL or N3 files" t)
+
+;; Turn on font lock when in n3 mode
+(add-hook 'n3-mode-hook
+          'turn-on-font-lock)
+
+(setq auto-mode-alist
+      (append
+       (list
+        '("\\.n3" . n3-mode)
+        )
+       auto-mode-alist))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; DIR Tree
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
