9b653d5797c427cc86f1463daa5cfd8095d26410
Guohui Xiao <gh.xiao@gmail.com>
2013-07-26 14:49:10 +0200

    enable xetex

--- init.el
+++ init.el
@@ -247,6 +247,12 @@
 ;; (add-to-list 'helm-completing-read-handlers-alist
 ;;              '(Tex-command-master . nil) )
 
+
+;;set XeTeX mode in TeX/LaTeX
+(add-hook 'LaTeX-mode-hook (lambda()
+                             (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex --synctex=1%(mode)%' %t" TeX-run-TeX nil t)))
+)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Uniquify
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
