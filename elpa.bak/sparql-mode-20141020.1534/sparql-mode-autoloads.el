ed54771407195c59fbf536a410c9ca9c7d7776b2
Guohui Xiao <gh.xiao@gmail.com>
2016-03-06 11:59:51 +0100

    remove elpa.bak

--- elpa.bak/sparql-mode-20141020.1534/sparql-mode-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; sparql-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "sparql-mode" "sparql-mode.el" (21645 30510
-;;;;;;  0 0))
-;;; Generated autoloads from sparql-mode.el
-
-(autoload 'sparql-mode "sparql-mode" "\
-Major mode for SPARQL-queries.
-\\{sparql-mode-map}
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("ob-sparql.el" "sparql-mode-pkg.el") (21645
-;;;;;;  30510 335470 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; sparql-mode-autoloads.el ends here
--- elpa.bak/sparql-mode-20141020.1534/sparql-mode-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "sparql-mode" "20141020.1534" "Edit and interactively evaluate SPARQL queries." 'nil :url "https://github.com/ljos/sparql-mode")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
